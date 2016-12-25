832b68ff4313a3ebb656c388951283f69dc857e3
david karapetyan <dkarapetyan@siftscience.com>
2016-12-24 19:20:04 -0800

    more type safety and componentized dot product

--- lib/cosine-similarity.ts
+++ lib/cosine-similarity.ts
@@ -1,21 +1,32 @@
 // instead of keeping the weights in the dot product we
 // can track the weight of each feature alongside the feature
 interface WeightedFeature<T> {
-    f: T;
-    w: number;
+    readonly f: T;
+    readonly w: number;
 }
 
 // our feature vector which we are going to use to do the cosine similarity calculation
 interface FeatureVector {
-    children: WeightedFeature<Array<string>>;
-    childrenLength: WeightedFeature<number>;
-    siblingsLength: WeightedFeature<number>;
-    parent: WeightedFeature<string>;
-    node: WeightedFeature<string>;
-    width: WeightedFeature<number>;
-    height: WeightedFeature<number>;
-    attributes: WeightedFeature<Array<string>>;
-    attributesLength: WeightedFeature<number>;
+    readonly children: WeightedFeature<Array<string>>;
+    readonly childrenLength: WeightedFeature<number>;
+    readonly siblingsLength: WeightedFeature<number>;
+    readonly parent: WeightedFeature<string>;
+    readonly node: WeightedFeature<string>;
+    readonly width: WeightedFeature<number>;
+    readonly height: WeightedFeature<number>;
+    readonly attributes: WeightedFeature<Array<string>>;
+    readonly attributesLength: WeightedFeature<number>;
+}
+
+// required when we are iterating over the partial dot product
+// components to get the final sum
+interface StringToNumber {
+    [index: string]: number;
+}
+
+// mapped types are pretty awesome
+type DotProductComponents = {
+    readonly [P in keyof FeatureVector]: number;
 }
 
 // now the cosine similarity of 2 feature vectors
@@ -32,38 +43,56 @@ function norm(featureVector: FeatureVector) {
 // just pointwise products of features
 function dotProduct(a: FeatureVector, b: FeatureVector) {
     // some helper functions
-    let pm = (a: any, b: any) => a === b ? 1 : 0;
-    let wm = <T>(a: WeightedFeature<T>, b: WeightedFeature<T>) => {
+    let pm = (a: any, b: any) => a === b ? 1 : 0; // (p)rimitive (m)atcher
+    let wm = <T>(a: WeightedFeature<T>, b: WeightedFeature<T>) => { // (w)eighted (m)atcher
         if (a.w != b.w) { throw "Can not compare features with different weights!"; }
         return a.f === b.f ? a.w : 0;
     };
-    let wp = <T>(a: WeightedFeature<T>, b: WeightedFeature<T>) => a.w * wm(a, b);
-    let wpp = (w: number, a: any, b: any) => w * pm(a, b);
-    let sum = wp(a.node, b.node) + wp(a.parent, b.parent) +
-        wp(a.childrenLength, b.childrenLength) +
-        wp(a.siblingsLength, b.siblingsLength) +
-        wp(a.attributesLength, b.attributesLength) +
-        wp(a.width, b.width) +
-        wp(a.height, b.height);
+    let wp = <T>(a: WeightedFeature<T>, b: WeightedFeature<T>) => a.w * wm(a, b); // (w)eighted (p)roduct
+    let wpp = (w: number, a: any, b: any) => w * pm(a, b); // (w)eighted (p)rimitive (p)roduct
+    // first the array dot products for children
+    let childrenSum = 0;
     let childIterationLength = Math.min(a.childrenLength.f, b.childrenLength.f);
     for (let index = 0; index < childIterationLength; index++) {
-        sum += wpp(a.children.w, a.children.f[index], b.children.f[index]);
+        childrenSum += wpp(a.children.w, a.children.f[index], b.children.f[index]);
     }
+    // now the dot product for attributes
     let attributeIterationLength = Math.min(a.attributesLength.f, b.attributesLength.f);
+    let attributeSum = 0;
     for (let index = 0; index < attributeIterationLength; index++) {
-        sum += wpp(a.attributes.w, a.attributes.f[index], b.attributes.f[index]);
+        attributeSum += wpp(a.attributes.w, a.attributes.f[index], b.attributes.f[index]);
+    }
+    // now the other components of the dot product, making it string indexable because 
+    // i want to iterate over the keys
+    let dotProductComponents: DotProductComponents & StringToNumber = {
+        attributes: attributeSum,
+        children: childrenSum,
+        childrenLength: wp(a.childrenLength, b.childrenLength),
+        parent: wp(a.parent, b.parent),
+        node: wp(a.node, b.node),
+        siblingsLength: wp(a.siblingsLength, b.siblingsLength),
+        attributesLength: wp(a.attributesLength, b.attributesLength),
+        width: wp(a.width, b.width),
+        height: wp(a.height, b.height)
+    };
+    // now index over the keys and add them up
+    let sum = 0;
+    for (let partialSumIndex in dotProductComponents) {
+        sum += dotProductComponents[partialSumIndex];
     }
     return sum;
 }
 
 // map a DOM element to its features
 function extractFeatures(node : HTMLElement): FeatureVector {
-    let wf = <T>(f: T, w: number): WeightedFeature<T> => { return {f: f, w: w} };
+    let wf = <T>(f: T, w: number): WeightedFeature<T> => { return {f: f, w: w} }; // (w)eighted (f)eature
     // we are going to use the name to find all the nodes with same name
-    let name: string = node.nodeName;
+    let name = node.nodeName;
     // first we iterate over the child nodes and add their names to a list
     let children: Array<string> = [];
-    for (let child of node.childNodes as any as Array<HTMLElement>) {
+    // NodeList can not be converted to array so must bypass typechecking by casting to any
+    let childNodes: Array<HTMLElement> = node.childNodes as any;
+    for (let child of childNodes) {
         children.push(child.nodeName);
     }
     // casting to avoid strict null check since it is not possible when comparing
@@ -94,8 +123,8 @@ function extractFeatures(node : HTMLElement): FeatureVector {
 // common set of transformations when trying to find matches for an element
 function matcher(anchor: HTMLElement) {
     let anchorFeatures = extractFeatures(anchor);
-    let matches = Array.prototype.slice.call(
-        document.querySelectorAll(anchorFeatures.node.f)) as Array<HTMLElement>;
+    let matches: Array<HTMLElement> = Array.prototype.slice.call(
+        document.querySelectorAll(anchorFeatures.node.f));
     let results = matches.map(element => {
         let elementFeatures = extractFeatures(element);
         let similarity = cosineSimilarity(anchorFeatures, elementFeatures);
